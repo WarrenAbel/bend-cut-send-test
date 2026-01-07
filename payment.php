24eb8db804bd5a5788ad0fc1575c760c3e81b34e
WarrenAbel <KalaiConsult@gmail.com>
2026-01-07 13:48:52 +0200

    Create payment.php file with payment processing logic in PHP.

--- payment.php
+++ payment.php
@@ -1,4 +1,19 @@
 <?php
-// Content converted to PHP from payment.html starts below
+// This file is for handling payment processing in PHP
 
-?>
+class Payment {
+    private $amount;
+
+    public function __construct($amount) {
+        $this->amount = $amount;
+    }
+
+    public function processPayment() {
+        // Logic to process payment goes here
+        echo "Processing payment of amount: " . $this->amount;
+    }
+}
+
+// Example of using the Payment class
+$payment = new Payment(100);
+$payment->processPayment();
